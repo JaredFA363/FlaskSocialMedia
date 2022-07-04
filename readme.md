fba3477749eec61108d157321140d238d4dba07e
Jared <jaredawiti2017@gmail.com>
2022-07-04 08:05:45 +0100

    Login amd sign in forms complete backend still incomplete

--- readme.md
+++ readme.md
@@ -8,4 +8,5 @@ A responsive social media website using Python-flask, Css , Html and Javascript.
 
 ## References
 [Responsive NavBar](https://www.youtube.com/watch?v=gXkqy0b4M5g) video
+<br>
 [Contact us page](https://www.youtube.com/watch?v=orBQesFBkXg) video
--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,39 @@
-from flask import Blueprint, render_template
+import email
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
     return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<p>LogOut</p>"
 
-@auth.route("/signup")
+@auth.route("/signup", methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        userName = request.form.get('userName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 4:
+            flash('Email must be greater than 3 characters', category='error')
+        elif len(userName) <= 2:
+            flash('Username must be greater than 1 characters', category='error')
+        elif password1 != password2:
+            flash('Passwords must match', category='error')
+        elif len(password1) < 7:
+            flash('Passwords must be greater than 7 characters', category='error')
+        else:
+            #add user to db
+            flash('Account created!', category='successful')
+            pass
+
     return render_template("signup.html")
 
